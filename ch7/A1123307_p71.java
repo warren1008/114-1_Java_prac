f1e04d11be4da2b3c15d4b1c5a6dd59dbac26f78
warren1008 <A1123307@mail.nuk.edu.tw>
2026-04-10 14:26:29 +0800

    delete failure file

--- ch7/A1123307_p71.java
+++
@@ -1,5 +0,0 @@
-public class A1123307_p71 {
-    public static void main(String[] args) {
-        
-    }
-}
--- ch7/A1123307_t72.java
+++
@@ -1,9 +0,0 @@
-public class A1123307_t72 {
-    public static void main(String[] args) {
-        
-    }
-    
-    public static void main(String[] args) {
-        
-    }
-}
